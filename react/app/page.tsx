e18811cc511d018e4f68c2b9c5766993ff080890
Matthijs217 <matthijstenbrink@gmail.com>
2025-10-31 11:29:55 +0100

    Next.js + supabase #3

--- react/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page() {
-  const data = await fetch('https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E&sort=name')
-  const posts = await data.json()
-  return (
-    <ul>
-      test
-    </ul>
-  )
-}
